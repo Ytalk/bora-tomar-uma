dee23242f4e9dd8fbe0c234f288780d08a50992e
Ytalk <ytalkkzk@gmail.com>
2023-11-20 03:51:00 -0300

    reformulação da criação de cerveja (adição de receita.java e modificação total de cervejaArtesanal.java). ademais, estoque foi compilado e agora há .class

--- estoque/cervejaArtesanal.java
+++ estoque/cervejaArtesanal.java
@@ -3,32 +3,21 @@ package estoque;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class cervejaArtesanal {
-private int volume; // quantidade de cerveja produziada
-private ArrayList <materiaPrima> materiasUtilizadas;
-private double custoTotal;
+public abstract class cervejaArtesanal{
+    private int volume; // quantidade de cerveja produziada
+    private double valor;
+    private ArrayList<materiaPrima> rótulo;
 
- public cervejaArtesanal(){
- this.materiasUtilizadas = new ArrayList<>();
- }
-
-    public void adicionarMateriaPrima(materiaPrima item){
-    materiasUtilizadas.add(item);
+    public cervejaArtesanal(receita receita){
+        this.valor = receita.getCusto() + (receita.getCusto() * 0.25);//margem de lucro de 25%
+        this.volume = receita.getPeso();//forma grosseira de calcular
     }
 
-    public List<materiaPrima> getMateriasUtilizadas() {
-    return materiasUtilizadas;
+    public int getVolume(){
+        return volume;
     }
 
-
-    public double calcularCustoTotal() {
-     custoTotal = 0;
-
-    for (materiaPrima materia : materiasUtilizadas) {
-        custoTotal += materia.getCusto();
+    public double getValor(){
+        return valor;
     }
-
-    return custoTotal;
-    }
-
 }
---
+++ estoque/receita.java
@@ -0,0 +1,54 @@
+package estoque;
+
+import java.util.ArrayList;
+
+import estoque.materiaPrima;
+
+public class receita{
+    private String nome;
+    private ArrayList<materiaPrima> ingredientes;
+
+    public receita(String nome){
+        this.nome = nome;
+        this.ingredientes = new ArrayList<materiaPrima>();
+    }
+
+    public void addIngrediente(materiaPrima ingrediente){
+        ingredientes.add(ingrediente);
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    /*public arrayList<materiaPrima> getReceita(){
+        return ingredientes;
+    }*/
+
+    public int getPeso(){
+        int pesoTotal = 0;
+
+        for(materiaPrima ingrediente : ingredientes){
+            pesoTotal += ingrediente.getPeso();
+        }
+        return pesoTotal;
+    }
+    
+    public double getCusto(){
+        double custoDaReceita = 0;
+
+        for(materiaPrima ingrediente : ingredientes){
+            custoDaReceita += ingrediente.getCusto();
+        }
+        return custoDaReceita;
+    }
+
+    public String listarReceita(){
+        String receita = "\n======================================================\n\t\tRECEITA" + nome.toUpperCase() + "======================================================\n\n";
+        
+        for(materiaPrima ingrediente : ingredientes){
+            receita = receita + ingrediente.getPeso() + " gramas de " + ingrediente.getDesc() + "\t-\t" + ingrediente.getCusto() + "\n";
+        }
+        return receita;
+    }
+}
